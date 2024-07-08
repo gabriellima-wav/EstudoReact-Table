46f7f5c1137aeb5e25af9adfae2fa7b94e3bbe80
gabriellima-wav <gabriellimamelo2302@gmail.com>
2024-07-08 13:12:10 -0300

    columns

---
+++ src/componets/columns.js
@@ -0,0 +1,32 @@
+export const COLUMNS = [
+    {
+        Headers: 'Id',
+        accessor: 'Id'
+    },
+
+    {
+        Headers: 'First Name',
+        accessor: 'First_name'
+    },
+
+    {
+        Headers: 'Last Name',
+        accessor: 'last_name'
+    },
+
+    {
+        Headers: 'Date of Birth',
+        accessor: 'date_of_birth'
+    },
+
+    {
+        Headers: 'Country',
+        accessor: 'Country'
+    },
+
+    {
+        Headers: 'Phone',
+        accessor: 'phone'
+    }
+
+]

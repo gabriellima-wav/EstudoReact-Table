9b91a4921fe1ff9d3460b2abbd9928b26102384d
gabriellima-wav <gabriellimamelo2302@gmail.com>
2024-07-09 12:44:43 -0300

    inicio do sorting

--- src/data/columns.js
+++ src/data/columns.js
@@ -36,3 +36,51 @@ export const COLUMNS = [
     }
 
 ]
+
+export const GROUPED_COLUMNS = [
+    {
+        Header: 'Id',
+        Footer: 'Id',
+        accessor: 'Id'
+    },
+    {
+        Header: 'Name',
+        Footer: 'Name',
+        columns: [
+            {
+                Header: 'First Name',
+                Footer: 'First Name',
+                accessor: 'First_name'
+            },
+        
+            {
+                Header: 'Last Name',
+                Footer: 'Last Name',
+                accessor: 'last_name'
+            },  
+]
+    },
+    {
+        Header: 'Info',
+        Footer: 'Info',
+        columns: [
+            {
+                Header: 'Date of Birth',
+                Footer: 'Date of Birth',
+                accessor: 'date_of_birth'
+            },
+        
+            {
+                Header: 'Country',
+                Footer: 'Country',
+                accessor: 'Country'
+            },
+        
+            {
+                Header: 'Phone',
+                Footer: 'Phone',
+                accessor: 'phone'
+            }
+        ]
+    }
+]
